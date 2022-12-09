31a0068c7dffcebfec9fa73bb89b61490139dec5
noyflaysher <noyflaysher12@gmail.com>
2022-12-09 13:32:35 +0200

    change server url

--- client/src/socket/SocketService.js
+++ client/src/socket/SocketService.js
@@ -10,7 +10,7 @@ export const SocketService={
 
 function init(){
     console.log("init");
-    socket=io("https://draw-and-guess-server2.onrender.com");
+    socket=io("http://localhost:3002");
 }
 
 function terminate(){
